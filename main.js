30911a0d6cdcc5b47af8e90fb2766f839c4a8bea
Jaque Lima ✿ <jaqueline.lima.jpl@gmail.com>
2022-05-05 18:15:23 -0300

    Criando menu.

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-function onScrooll(){
-  if(scrollY > 0){
-      navigation.classList.add('scroll')//navigation é um objeto/ lista de classes que é um objeto por isso usa .
-  } else{
+function onScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
     navigation.classList.remove('scroll')
   }
+}
+//funções são agrupamentos de códigos e objetos tem propriedades e funcionalidades.
+ 
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
 }
